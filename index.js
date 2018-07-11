f6db33adcbfe9af3050fde41ae9b27b19c680818
sofia ongele <sofiaongele@gmail.com>
2018-07-11 18:04:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(street) {
+  let hq = 42 
+  return Math.abs(42 - street)
+}
+
+function distanceFromHqInFeet(street) {
+  return distanceFromHqInBlocks(street) * 264
+}
