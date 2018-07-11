49135129907e626a5d4a2e10c195e20401a77075
sofia ongele <sofiaongele@gmail.com>
2018-07-11 18:19:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function calculatesFarePrice(first, second) {
   if (distance <= 400) {
     return 0
   } else if (400 < distance && distance < 2000) {
-    return distance * 0.02
+    return distance * 0.01
   } 
 }
